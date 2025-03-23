4aa0207ae055164cb1225aebcf637116689f7e1e
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2025-03-22 21:28:50 -0500

    create clase 04 python

---
+++ python_course/Clase04/clase04.py
@@ -0,0 +1,14 @@
+number_01 = int(input("Ingrese 1er Numero : "))
+number_02 = int(input("Ingrese 2do Numero : "))
+
+suma = number_01 + number_02
+resta = number_01 - number_02
+multiplicacion = number_01 * number_02
+division = number_01 / number_02
+
+print()
+print("Operaciones Aritmeticas")
+print("La suma es : ", suma)
+print("La resta es : ", resta)
+print("La multiplicacion : ", multiplicacion)
+print("La division es : ", division)
